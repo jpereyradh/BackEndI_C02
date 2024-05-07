1bdbf79db730a4130b14069d701088015106d4a9
jpereyradh <jorge.pereyra@digitalhouse.com>
2024-05-06 21:55:58 -0300

    C5

---
+++ Clase 5- Patron Flyweight/C5_PatronFlyweight/src/presencial/Computadora.java
@@ -0,0 +1,17 @@
+package presencial;
+
+public class Computadora {
+    private int ram;
+    private int discoDuro;
+    private static int contador=0;
+
+    public Computadora(int discoDuro, int ram) {
+        this.discoDuro = discoDuro;
+        this.ram = ram;
+        contador++;
+        System.out.println(cantidadDeComputadorasCreadas());
+    }
+    public double cantidadDeComputadorasCreadas(){
+        return contador;
+    }
+}
