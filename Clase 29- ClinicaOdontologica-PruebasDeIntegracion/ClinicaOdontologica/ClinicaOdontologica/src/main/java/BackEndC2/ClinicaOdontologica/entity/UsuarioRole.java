c2eaf78ae8375116ae9c7f47f775697f4307d699
Jorge Pereyra <jorge.pereyra@DH.digitalhouse.com>
2024-06-19 21:50:30 -0300

    C29ClinicaOdontologica-Test

---
+++ Clase 29- ClinicaOdontologica-PruebasDeIntegracion/ClinicaOdontologica/ClinicaOdontologica/src/main/java/BackEndC2/ClinicaOdontologica/entity/UsuarioRole.java
@@ -0,0 +1,5 @@
+package BackEndC2.ClinicaOdontologica.entity;
+
+public enum UsuarioRole {
+    ROLE_USER, ROLE_ADMIN
+}
---
+++ Clase 29- ClinicaOdontologica-PruebasDeIntegracion/ClinicaOdontologica/ClinicaOdontologica/src/main/java/BackEndC2/ClinicaOdontologica/repository/OdontologoRepository.java
@@ -0,0 +1,11 @@
+package BackEndC2.ClinicaOdontologica.repository;
+
+import BackEndC2.ClinicaOdontologica.entity.Odontologo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface OdontologoRepository extends JpaRepository<Odontologo,Long> {
+
+    Optional<Odontologo> findByMatricula(String matricula);
+}
